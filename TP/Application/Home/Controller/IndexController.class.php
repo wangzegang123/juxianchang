d095c9d516c608fe35e190a0493d1c0d6c43c3b1
wangzegang123456 <2205339818@qq.com>
2018-01-08 19:23:54 +0800

    Signed-off-by: wangzegang123456 <2205339818@qq.com>

--- TP/Application/Home/Controller/IndexController.class.php
+++ TP/Application/Home/Controller/IndexController.class.php
@@ -5,6 +5,7 @@ class IndexController extends Controller {
     public function index(){
           $this->display();
     }
+<<<<<<< HEAD
     public function activity($res){//根据用户查询活动的所有信息
         $Model = new \Think\Model();
         //计算参与人数,活动用户表使用 count()
@@ -156,5 +157,24 @@ class IndexController extends Controller {
             $this->redirect("Login/login");
         }
 
+=======
+    public function person_activity(){//我的活动页面列表
+        /*1、判断是否登录，登录获取保存session里的用户id，未登录跳登录页面
+         * 2、查询活动表，（查询此用户购买所有活动）
+         * 3、如果没有公共头部，查询用户表（查询此用户所有信息）
+         * */
+        $activity=M("activity");
+        $res=$activity->where("member_id=1")->select();//查询此用户购买所有活动
+//        var_dump($activity);
+//        die;
+//        $sql="SELECT * from jxc_member WHERE member_id=1";
+//        $info=
+        $user=M("member");
+        $info=$user->where('member_id=1')->select();//查询此用户所有信息
+
+        $this->assign("res",$res);
+        $this->assign("res",$user);
+        $this->display("person_activity");
+>>>>>>> bb5b7dd... Signed-off-by: wangzegang123456 <2205339818@qq.com>
     }
 }
