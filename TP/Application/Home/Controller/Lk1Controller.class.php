00aa797acaa2b2b0c28774e1654e5925a69e0177
wangzegang123456 <2205339818@qq.com>
2018-01-12 10:13:12 +0800

    刘凯

---
+++ TP/Application/Home/Controller/Lk1Controller.class.php
@@ -0,0 +1,56 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class Lk1Controller extends Controller {
+    public function index(){
+        $this->display('Home/index');
+    }
+    public function index2(){
+        $this->display('Home/index2');
+    }
+    public function index3(){
+        $this->display('Home/index3');
+    }
+    public function lkone(){
+        $this->display('Home/lkone');
+    }
+    public function lktwo(){
+        $this->display('Home/lktwo');
+    }
+    public function lktheer(){
+        $this->display('Home/lktheer');
+    }
+    public function lkfour(){
+        $this->display('Home/lkfour');
+    }
+    public function lkfive(){
+        $this->display('Home/lkfive');
+    }
+    public function lksix(){
+        $this->display('Home/lksix');
+    }
+    public function lkseven(){
+        $this->display('Home/lkseven');
+    }
+    public function lkeight(){
+        $this->display('Home/lkeight');
+    }
+    public function lknane(){
+        $this->display('Home/lknane');
+    }
+    public function lkten(){
+        $this->display('Home/lkten');
+    }
+    public function six(){
+        $this->display('Home/six');
+    }
+    public function nane(){
+        $this->display('Home/nane');
+    }
+    public function lk(){
+        $this->display('Home/lk');
+    }
+    public function ten(){
+        $this->display('Home/ten');
+    }
+}
