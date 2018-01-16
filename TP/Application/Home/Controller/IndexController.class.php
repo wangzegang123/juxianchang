631bb780dea239f78d329e07a72abad62d6c67be
wangzegang123456 <2205339818@qq.com>
2018-01-16 14:27:08 +0800

    Squashed commit of the following:
    commit ea137c513ecd838894b34531c23f664e43822b3d
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Tue Jan 16 09:44:54 2018 +0800
    
        新建活动
    
    commit 66f46b5a0e0d70e2440a7914e2311d2ad63794a2
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 19:37:21 2018 +0800
    
        index
    
    commit ac873c122b84b42800cac8c34db4cc40b388d691
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 19:33:25 2018 +0800
    
        sc
    
    commit dd6ad9a06b50fc9bc6536a00e4bbdd6f2d09d6a7
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 19:28:44 2018 +0800
    
        IndexController.class.php
    
    commit 09f5544a3cfc9b4727caed1577bd612bb89f7eb7
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 18:53:59 2018 +0800
    
        注册
    
    commit 11d7a394e510c31fbf668e5edb7a38ae884f3da9
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Fri Jan 12 10:33:12 2018 +0800
    
        吞吞吐吐
    
    commit 73421e1e7b090d3ee50ae9a4be14c346608340f4
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Fri Jan 12 10:32:59 2018 +0800
    
        y6
    
    commit 237f5d13885a36f4d1b9259ae85df36a607412e5
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 8 20:39:10 2018 +0800
    
        公共部分继承
    
    commit 516b9ba8598f5d91dbcd9cb71529b1ec3ddfff8c
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 18:30:49 2018 +0800
    
        ding
    
    commit 889eb2ab67d533bee6e187ce58dbdf8b230396ce
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 15 10:09:55 2018 +0800
    
        案例
    
    commit 73e117389129847536ab1de464b9458b75eaae4b
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Fri Jan 12 10:11:52 2018 +0800
    
        zhuye
    
    commit 4f3c3b55d320fb12cc813d69b4032b7b4644aa83
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Thu Jan 11 14:09:13 2018 +0800
    
        1
    
    commit 60014d9bc8e3ef4b3a2df70d61c641bedac64f23
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Thu Jan 11 10:23:09 2018 +0800
    
        123123
    
    commit e6e2fefcc74e336dcde0e2a65565ce409db499d0
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Wed Jan 10 16:33:20 2018 +0800
    
        56+
    
    commit d7fb6396e6a038a957d37f9f429e28c54a75dfad
    Merge: 1a097b2 fa03157
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Wed Jan 10 14:37:33 2018 +0800
    
        Merge branch 'master' of https://github.com/wangzegang123/juxianchang
    
        Conflicts:
        	TP/Application/Runtime/Logs/Home/18_01_08.log
    
    commit fa0315791f50972c50610a9c5839ae7145858dfe
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Tue Jan 9 15:26:05 2018 +0800
    
        修改js路径
    
    commit 21c76c355443d4607d370acf1a262e924e9de327
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 8 20:39:10 2018 +0800
    
        公共部分继承
    
    commit 313e9a1d744747f5ed5a178d3a8071f35d565d15
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 8 19:54:05 2018 +0800
    
        ceshi
    
    commit 721747ad0b2ecbb043e9e10f057f5740b1c6ca5c
    Author: wangzegang123456 <2205339818@qq.com>
    Date:   Mon Jan 8 19:23:54 2018 +0800
    
        Signed-off-by: wangzegang123456 <2205339818@qq.com>
    
    Conflicts:
    	TP/Application/Home/Controller/IndexController.class.php
    	TP/Application/Home/Controller/LkController.class.php
    	TP/Application/Home/View/Home/chongzhi.html
    	TP/Application/Home/View/Home/details.html
    	TP/Application/Home/View/Home/zhyue.html
    	TP/Application/Home/View/Index/activity_person.html
    	TP/Application/Home/View/public_part/public_part.html
    	TP/Application/Runtime/Cache/Home/45e9777793de22efc3504b7097315bf4.php
    	TP/Application/Runtime/Cache/Home/9a98843e7ef63110b3f5b8247bf08016.php
    	TP/Application/Runtime/Logs/Home/18_01_08.log
    	TP/Application/Runtime/Logs/Home/18_01_10.log
    	TP/Public/Home/js/b.js

--- TP/Application/Home/Controller/IndexController.class.php
+++ TP/Application/Home/Controller/IndexController.class.php
@@ -6,6 +6,7 @@ class IndexController extends Controller {
           $this->display();
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function activity($res){//根据用户查询活动的所有信息
         $Model = new \Think\Model();
         //计算参与人数,活动用户表使用 count()
@@ -158,6 +159,8 @@ class IndexController extends Controller {
         }
 
 =======
+=======
+>>>>>>> 8e09558... Squashed commit of the following:
     public function person_activity(){//我的活动页面列表
         /*1、判断是否登录，登录获取保存session里的用户id，未登录跳登录页面
          * 2、查询活动表，（查询此用户购买所有活动）
@@ -175,6 +178,163 @@ class IndexController extends Controller {
         $this->assign("res",$res);
         $this->assign("res",$user);
         $this->display("person_activity");
+<<<<<<< HEAD
 >>>>>>> bb5b7dd... Signed-off-by: wangzegang123456 <2205339818@qq.com>
+=======
+=======
+    public function activity($res){//根据用户查询活动的所有信息
+        $Model = new \Think\Model();
+        //计算参与人数,活动用户表使用 count()
+        //$sql="select count('fans_id') as nums,f.activity_id from jxc_activity_fans as f where f.activity_id in(select a.activity_id from jxc_activity as a where a.member_id=1) group by f.activity_id";
+        foreach($res as $k=>$v){
+            // var_dump($v['activity_id']);
+            $sql1="select  * from jxc_activity_fans where activity_id={$v['activity_id']}";
+            $peo1=$Model->query($sql1);
+            if(count($peo1)){
+                $p1[$k]=$peo1;
+            }else{
+                $p1[$k]=null;
+            }
+            $res[$k]['num']=$num[$k]=count($p1[$k]);
+        }
+        //计算签到人数
+        foreach($res as $kk=>$vv){
+            // var_dump($v['activity_id']);
+            $sql2="select  * from jxc_activity_fans where activity_id={$vv['activity_id']} and fans_sign=1";
+            $peo2=$Model->query($sql2);
+            if(count($peo2)){
+                $p2[$kk]=$peo2;
+            }else{
+                $p2[$kk]=null;
+            }
+            $res[$kk]['sign']=count($p2[$kk]);
+        }
+        //计算上墙消息条数
+        foreach($res as $k3=>$v3){
+            // var_dump($v['activity_id']);
+            $sql3="select  * from jxc_wall where activity_id={$v3['activity_id']} and wall_active=1";
+            $peo3=$Model->query($sql3);
+
+            if(count($peo3)){
+                $p3[$k3]=$peo3;
+            }else{
+                $p3[$k3]=null;
+            }
+            $res[$k3]['wall']=count($p3[$k3]);
+        }
+        //计算霸屏上墙消息条数
+        foreach($res as $k4=>$v4){
+            // var_dump($v['activity_id']);
+            $sql4="select  * from jxc_wall where activity_id={$v4['activity_id']} and wall_active=2";
+            $peo4=$Model->query($sql4);
+            if(count($peo4)){
+                $p4[$k4]=$peo4;
+            }else{
+                $p4[$k4]=null;
+            }
+            $res[$k4]['screen']=count($p4[$k4]);
+        }
+
+        return $res;
+    }
+    public function activity_person(){//我的活动页面列表
+        if(isset($_SESSION['mid'])){
+            $mid=$_SESSION['mid'];
+            $activity=M("activity");
+            if(!empty($_GET)){
+                $aname=$_GET['activity_name'];
+                $mname=$_GET['member_name'];
+                $sql="select a.* FROM jxc_activity as a INNER JOIN jxc_member as m ON a.member_id=m.member_id WHERE a.activity_theme LIKE '%$aname%' AND m.member_id=$mid AND m.member_name LIKE '%$mname%'";
+                $Model = new \Think\Model();
+                $res=$Model->query($sql);
+                $res1=$this->activity($res);
+
+            }else{
+                $res=$activity->where("member_id=$mid")->select();
+                $res1=$this->activity($res);
+
+            }
+            $this->assign("res",$res1);
+            $this->display("activity_person");
+
+        }else{
+            $this->redirect("Login/login");
+        }
+
+
+    }
+    public function activity_start(){//开始活动页面列表
+        if(isset($_SESSION['mid'])){
+            $mid=$_SESSION['mid'];
+            $activity=M("activity");
+            $time=time();
+            if(!empty($_GET)){
+                $aname=$_GET['activity_name'];
+                $mname=$_GET['member_name'];
+                $sql="select a.* FROM jxc_activity as a INNER JOIN jxc_member as m ON a.member_id=m.member_id WHERE a.activity_theme LIKE '%$aname%' AND m.member_id=$mid AND m.member_name LIKE '%$mname%' AND a.activity_start_time<='$time' and a.activity_end_time>='$time'";
+                $Model = new \Think\Model();
+                $res=$Model->query($sql);
+                $res1=$this->activity($res);
+            }else{
+                $res=$activity->where("member_id=$mid and activity_start_time <= $time and activity_end_time >= $time")->select();
+                $res1=$this->activity($res);
+            }
+
+            $this->assign("res",$res1);
+            $this->display("activity_start");
+
+        }else{
+            $this->redirect("Login/login");
+        }
+    }
+    public function activity_notstart(){//未开始活动页面列表
+        if(isset($_SESSION['mid'])){
+            $mid=$_SESSION['mid'];
+            $activity=M("activity");
+            $time=time();
+            if(!empty($_GET)){
+                $aname=$_GET['activity_name'];
+                $mname=$_GET['member_name'];
+                $sql="select a.* FROM jxc_activity as a INNER JOIN jxc_member as m ON a.member_id=m.member_id WHERE a.activity_theme LIKE '%$aname%' AND m.member_id=$mid AND m.member_name LIKE '%$mname%' AND a.activity_start_time>'$time'";
+                $Model = new \Think\Model();
+                $res=$Model->query($sql);
+                $res1=$this->activity($res);
+            }else{
+                $res=$activity->where("member_id=$mid and activity_start_time > $time")->select();
+                $res1=$this->activity($res);
+            }
+
+            $this->assign("res",$res1);
+            $this->display("activity_notstart");
+
+        }else{
+            $this->redirect("Login/login");
+        }
+    }
+    public function activity_end(){//已结束活动页面列表
+        if(isset($_SESSION['mid'])){
+            $mid=$_SESSION['mid'];
+            $activity=M("activity");
+            $time=time();
+            if(!empty($_GET)){
+                $aname=$_GET['activity_name'];
+                $mname=$_GET['member_name'];
+                $sql="select a.* FROM jxc_activity as a INNER JOIN jxc_member as m ON a.member_id=m.member_id WHERE a.activity_theme LIKE '%$aname%' AND m.member_id=$mid AND m.member_name LIKE '%$mname%' AND a.activity_end_time<'$time'";
+                $Model = new \Think\Model();
+                $res=$Model->query($sql);
+                $res1=$this->activity($res);
+            }else{
+                $res=$activity->where("member_id=$mid and activity_end_time < $time")->select();
+                $res1=$this->activity($res);
+            }
+            $this->assign("res",$res1);
+            $this->display("activity_end");
+
+        }else{
+            $this->redirect("Login/login");
+        }
+
+>>>>>>> ea137c513ecd838894b34531c23f664e43822b3d
+>>>>>>> 8e09558... Squashed commit of the following:
     }
 }
